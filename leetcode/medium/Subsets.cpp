819e12f19d72d7bd2d0fc78b001b0c34fbd3b357
“Xinfan <k19002026@kcl.ac.uk>
2021-04-20 16:16:48 +0100

    add comments

--- leetcode/medium/Subsets.cpp
+++ leetcode/medium/Subsets.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+/*
+Problem: 
+    Given an integer array nums of unique elements, return all possible subsets (the power set).
+    The solution set must not contain duplicate subsets. Return the solution in any order. 
+*/
+
 class Solution {
 public:
      vector<vector<int>> sets(vector<int>& nums, int next, vector<int> &current,              vector<vector<int>> &powerSet) {
@@ -18,6 +24,7 @@ public:
         }
         return powerSet;
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> powerSet;
         vector<int> current;
--- leetcode/medium/TwoCityScheduling.cpp
+++ leetcode/medium/TwoCityScheduling.cpp
@@ -3,7 +3,13 @@
 #include <sting>
 
 using namespace std;
- 
+
+/* 
+Problem: 
+    A company is planning to interview 2n people. Given the array costs where costs[i] = [aCosti, bCosti], the cost of flying the ith person to city a is aCosti, and the cost of flying the ith person to city b is bCosti.
+    Return the minimum cost to fly every person to a city such that exactly n people arrive in each city. 
+*/
+
  //sort vector according two bigger diffence
  bool wayToSort(vector<int> i,vector<int> j){
         int differenceA = abs(i[0] - i[1]);
